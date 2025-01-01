f6759935ad9a90c34f6df2cf92db4e67077b052b
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-02 00:16:47 +0600

    number.js file added

---
+++ number.js
@@ -0,0 +1,19 @@
+var orangePrice = 40;
+var bananaPrice = 0.5;
+var applePrice = parseInt("30");
+console.log(orangePrice + bananaPrice +applePrice)
+console.log(typeof orangePrice)
+console.log( typeof bananaPrice)
+
+
+var soltPrice = 50;
+soltPrice = 70;
+// var howManeyKgsSolt = parseInt('70.10');
+var howManeyKgsSolt = parseFloat('70.9');
+console.log(soltPrice + howManeyKgsSolt);
+console.log(typeof howManeyKgsSolt)
+
+var first = 0.1;
+var second = 0.3;
+var total = first + second;
+console.log(total)
