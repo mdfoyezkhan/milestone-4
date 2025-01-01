8d35f178a096b53020b77308a34803b645ca9e65
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-02 01:37:31 +0600

    new file added

---
+++ mixedop.js
@@ -0,0 +1,5 @@
+var first = "foyej";
+var second = "khan";
+var fullName = first + second;
+console.log(fullName)
+console.log('40'-10)
---
+++ shorthend.js
@@ -0,0 +1,18 @@
+var price =35;
+// price = price + 10;
+price += 15;
+console.log(price)
+var fridge = 3000;
+// fridge = fridge - 1000;
+// fridge =fridge +500;
+fridge -=1000;
+fridge += 500;
+fridge -= 700;
+console.log(fridge)
+
+var onionPrice = 40;
+onionPrice =onionPrice * 5;
+onionPrice -= 20;
+onionPrice += 500;
+onionPrice /= 5;
+console.log(onionPrice)
