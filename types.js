75e884cd99e58efdfb5cf8024c34f5dd02038c62
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-01 23:20:23 +0600

    types.js file added

---
+++ types.js
@@ -0,0 +1,15 @@
+// string type data
+var address = "kamal atatuk road"
+var country = "bangladesh"
+var village = "amrita"
+village = "kagawra"
+console.log(address + country + village)
+//boolean type data
+var isRich =true;
+var areYouReaddingToday =true;
+console.log(areYouReaddingToday)
+//number type data
+var banana = 30;
+console.log(typeof banana)
+console.log(banana)
+
