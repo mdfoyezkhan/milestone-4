f5b80459b9ca22b4f58e44180f52f1458ff167f3
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-02 00:56:13 +0600

    operation.js file added

---
+++ operation.js
@@ -0,0 +1,17 @@
+var eggPrice = 50;
+var onionPrice = 70;
+console.log(onionPrice % eggPrice)
+
+console.log(10%3)
+
+var money = 500;
+money = 1000;
+money = money - 300;
+money = money + 50;
+money = money % 20;
+console.log(money)
+
+var newPrice = money + 20;
+var discountedPrice = newPrice - 10;
+console.log(newPrice)
+console.log(discountedPrice)
