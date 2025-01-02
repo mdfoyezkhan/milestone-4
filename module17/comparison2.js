c3588efd07d5f44f338a917727565e4a8fa45b38
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-02 22:59:34 +0600

    comparison2.js file added

---
+++ module17/comparison2.js
@@ -0,0 +1,11 @@
+// console.log(20 >= 20);
+var soltPrice = 200;
+var sugerPrice = 100;
+// console.log(soltPrice < sugerPrice)
+// console.log(soltPrice >= sugerPrice);
+// console.log(5 > 10)
+// console.log(5 < 10)
+// console.log(5 == 10)
+// console.log(15 >= 10)
+// console.log(15 === '10')
+console.log(15 !== '10')
