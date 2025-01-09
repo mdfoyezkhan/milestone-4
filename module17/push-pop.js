c6061cca3b0eb67a4991d4c055078332cfed88ed
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 01:09:10 +0600

    push pop.js file added

---
+++ module17/push-pop.js
@@ -0,0 +1,8 @@
+const listName2 = ['foyej khan', 'emon khan' , 'salman khan', 'parvej khan', 'afzal khan' ];
+listName2.push('kibria khan')
+listName2.push('ismail khan')
+// listName2.pop()
+// listName2.pop()
+// listName2.shift()
+listName2.unshift('monir khan')
+console.log(listName2)
