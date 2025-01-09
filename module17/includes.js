0d2deaae51c284a1c1ce5db079d6bd651dee6966
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 01:34:08 +0600

    includes.js file added

---
+++ module17/includes.js
@@ -0,0 +1,10 @@
+const listName2 = ['foyej khan', 'emon khan' , 'salman khan', 'parvej khan', 'afzal khan' ];
+console.log(listName2.includes('sarjan khan'))
+if (listName2.includes('foyej khan')){
+    var party = "we are going to testaurant";
+    console.log(party)
+}
+else{
+    var noParty = "we dont go to testaurant"
+    console.log(noParty)
+}
