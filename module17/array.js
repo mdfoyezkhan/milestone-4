75ba750e83968f768b7ac261d91a14a71ec5b0a9
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 00:00:29 +0600

    new code addede

--- module17/array.js
+++ module17/array.js
@@ -3,7 +3,14 @@ if (number <= 30){
     const discount = number * 10 / 100;
     const payAmount = number - discount;
     console.log(payAmount)
+
 }
+else if(number >=15 ){
+    const discount = number * 5 / 100;
+    const payAmount =number - discount;
+    console.log(payAmount)
+}
+
 else{
     console.log(number)
 }
