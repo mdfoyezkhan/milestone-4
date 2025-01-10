fe8c14cce487b46ced666056f7f502bb081353de
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 22:02:39 +0600

    new module18 added

---
+++ module18/for-of.js
@@ -0,0 +1,10 @@
+const number = [12, 13, 14, 15, 16, 17];
+const number1 = ['foyej khan', 'emon khan','salman khan']
+console.log(number)
+console.log(number1)
+for(const num of number){
+    console.log(num)
+}
+for(const num1 of number1){
+    console.log(num1)
+}
