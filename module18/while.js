d7e34ec1c03b178b05b5cc098438f42380b64d2e
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 22:43:23 +0600

    new file while.js added white some code

---
+++ module18/while.js
@@ -0,0 +1,15 @@
+let number = 3;
+console.log(number)
+while(number >= 10){
+    console.log('hello', number)
+    number = number + 1;
+}
+if(number >= 5){
+    while(number <= 5){
+        console.log('hello world', number);
+        number = number +1;
+    }
+}
+else{
+    console.log('something went wrong')
+}
