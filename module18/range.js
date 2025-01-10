30019c1274f0c63af47e842dd64d2b2a88109975
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-10 23:27:26 +0600

    new file range and sum added this commit

---
+++ module18/range.js
@@ -0,0 +1,12 @@
+let number = 1;
+// console.log(number)
+while(number <= 10){
+    console.log(number);
+
+    number++;
+}
+let num = 50;
+while(num <= 100){
+    console.log(num);
+    num++;
+}
---
+++ module18/sum.js
@@ -0,0 +1,15 @@
+let num = 1;
+let sum = 0;
+while(num <= 10){
+    sum = sum + num;
+    console.log('sum',sum);
+    num++;
+}
+let nun4 = 1;
+while(nun4 <= 10){
+    console.log(num);
+    if(nun4 % 2 === 0){
+       console.log('even number',nun4)
+    }
+    nun4++;
+}
