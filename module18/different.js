f7efac73afe8bdc20216e6d4b4fff28c07c15af5
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-12 23:12:23 +0600

    more code added in different.js file

--- module18/different.js
+++ module18/different.js
@@ -1,6 +1,20 @@
-for(let i = 1; i <= 30; i++){
-    if(i % 5 ===1){
-        console.log(i)
-    }
+// for(let i = 1; i <= 30; i++){
+//     if(i % 5 ===1){
+//         console.log(i)
+//     }
     
+// }
+for (let n = 0; n <= 30; n++){
+    if(n % 10 ===1){
+        console.log('hello world',n)
+    }
 }
+let total = 10;
+for(m = 1; m <= 100; m++){
+    if(m % 20 === 1){
+        console.log('how are you all', m)
+        total = total + m;
+        console.log("total amount", total)
+    }
+}
+console.log('invest amount', total)
