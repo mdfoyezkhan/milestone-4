b3f5beabcf8f60d35104f1955cba790914fa3199
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-13 22:15:02 +0600

    some code added

--- module19/string.js
+++ module19/string.js
@@ -5,3 +5,5 @@ console.log(typeof email,typeof division,typeof distric)
 const number =[12, 13, 14, 15, 16];
 console.log(number.length)
 console.log(number[2])
+const capital ='dhaka';
+console.log(capital[3])
