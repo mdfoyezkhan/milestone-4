631330df4e768a450854daef138ca13b72bb82be
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-13 22:32:09 +0600

    case.js file whit some code added

---
+++ module19/case.js
@@ -0,0 +1,11 @@
+const school ='ragib rabeya school and colleg';
+const subject ='Bangla';
+const book ='Bangla'
+console.log(book)
+console.log(book.toLowerCase())
+if(subject.toUpperCase() === book.toUpperCase()){
+    console.log('i am reading bangla book everyday')
+}
+else{
+    console.log('i will not reading')
+}
