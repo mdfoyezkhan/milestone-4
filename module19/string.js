42638a807ea9dab18dd6e9f9888b673d533dd8a0
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-13 22:07:36 +0600

    some array code added

--- module19/string.js
+++ module19/string.js
@@ -2,3 +2,6 @@ const email ='foyejkhan@gmail.com';
 const division = "hobigonj";
 const distric ="sylhet "
 console.log(typeof email,typeof division,typeof distric)
+const number =[12, 13, 14, 15, 16];
+console.log(number.length)
+console.log(number[2])
