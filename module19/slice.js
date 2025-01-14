28acf043628d195c061f1114fcb7866424153fa1
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-14 22:41:27 +0600

    some code added this commit

--- module19/slice.js
+++ module19/slice.js
@@ -4,4 +4,10 @@ console.log(part)
 
 const sentence = 'i am a good and hardworking person';
 console.log(sentence.split(''));
-console.log(sentence.split('a'))
+console.log(sentence.split('a'));
+
+const friendsStr = 'salman, emon, jobber, sanam, arman, samaun';
+const friends = friendsStr.split(',')
+console.log(friends)
+
+const realFriends = ''
