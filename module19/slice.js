cf252d744df138befe1d44bd4d05c155da5225a5
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-14 22:32:30 +0600

    more code added in slice.js file

--- module19/slice.js
+++ module19/slice.js
@@ -1,3 +1,7 @@
 const address = 'hobigonj';
 const part = address.slice(2, 5);
 console.log(part)
+
+const sentence = 'i am a good and hardworking person';
+console.log(sentence.split(''));
+console.log(sentence.split('a'))
