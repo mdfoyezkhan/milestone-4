8f96f4002066b5b6d7bcbc95e3139785dfdb6edf
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-15 00:06:51 +0600

    property.js file whit some code added this commit

---
+++ module19/property.js
@@ -0,0 +1,11 @@
+const person = {
+    name:'foyej khan',
+    age:22,
+    profession:'web developer',
+    salary:25000,
+    married:false
+}
+console.log(person)
+console.log(person.profession)
+const income = person.salary;
+console.log(income)
