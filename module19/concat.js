5d900e679125a2fd88073b02e0ccf4508fecaf97
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-14 22:58:46 +0600

    more code added

--- module19/concat.js
+++ module19/concat.js
@@ -2,3 +2,5 @@ const fastName ='foyej';
 const lastName ='khan';
 const fullName = fastName + ' ' +lastName;
 console.log(fullName)
+const fullName2 =fastName.concat(' ').concat(lastName);
+console.log(fullName)
