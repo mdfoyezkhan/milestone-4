2ec1ad80726e3411395fb703e8151e8ee7e2b537
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-14 23:03:18 +0600

    includes code added this commit

--- module19/concat.js
+++ module19/concat.js
@@ -4,3 +4,4 @@ const fullName = fastName + ' ' +lastName;
 console.log(fullName)
 const fullName2 =fastName.concat(' ').concat(lastName);
 console.log(fullName)
+console.log(lastName.includes('foyej'))
