9a60a7fc0f416aa9fcea0359b43e47ba831d1f99
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-01-15 22:27:51 +0600

    new module19.5 and looping.js file with some code added this commit

---
+++ module19.5/looping.js
@@ -0,0 +1,22 @@
+const friends = ['foyej', 'salman', 'emon', 'arman']
+for(const friend of friends){
+    console.log(friend)
+}
+for(let i = 0; i < friends.length; i++){
+    console.log(i)
+    console.log(friends[i])
+}
+const numbers =[12, 13, 114, 145, 167, 123, 135, 1356,1235]
+for(let n = 0; n < numbers.length; n++){
+    console.log(numbers[n])
+}
+let b = 0;
+while(b < friends.length){
+    console.log(friends[b])
+    b++;
+}
+let x = 0;
+while(x < friends.length){
+    console.log(friends[x]);
+    x++;
+}
