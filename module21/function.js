39e867c3fdf028c2187808f83f87553cebeed7f7
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-02 19:20:20 +0600

    some new code added this commit

--- module21/function.js
+++ module21/function.js
@@ -2,3 +2,13 @@ function productList(){
     console.log('this is new product')
 }
 productList()
+function productPrice(){
+    const price = 50;
+    if(price>= 60){
+        console.log('this is for you')
+    }
+    else{
+        console.log(price)
+    }
+}
+productPrice()
