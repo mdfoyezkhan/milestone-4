f8c4c7c1cfa7af8ebe47955b1826b553adb9b3b5
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-02 22:08:44 +0600

    some code added in return.js file

--- module21/return.js
+++ module21/return.js
@@ -6,3 +6,12 @@ function tenTimes(number){
 tenTimes(5);
 const output = tenTimes(5);
 console.log('output',output)
+
+function cutHalf(number){
+    const hulf = number / 2;
+    return hulf;
+
+}
+cutHalf(50)
+const diff =cutHalf(50);
+console.log(diff)
