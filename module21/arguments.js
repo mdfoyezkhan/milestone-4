ea05bf345b8501264873fb5e829bcd7555c59211
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-02 20:36:53 +0600

    new file arguments.js added with some code

---
+++ module21/arguments.js
@@ -0,0 +1,16 @@
+function doubelIt(number){
+    const doubelItMan = number * 2;
+    console.log('this double man',doubelItMan);
+}
+doubelIt(10)
+console.log('-----------------')
+doubelIt(50)
+console.log('-----------------')
+const man = 30;
+doubelIt(man)
+console.log('-----------------')
+const man2 = 40;
+const jugfol = man2 + 30;
+doubelIt(jugfol)
+
+console.log('-----------------')
