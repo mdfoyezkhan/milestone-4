da2cc574319b296a443fcdab700d394cbbe3f04c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-02 20:12:05 +0600

    some code added for jugfol

--- module21/parameter.js
+++ module21/parameter.js
@@ -11,3 +11,10 @@ function borgoMeter(num1,num2){
     console.log('this is borgo meter', total)
 }
 borgoMeter(20,77)
+console.log('----------------');
+function forJugFol(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total)
+    console.log('this is total parameter jugfol',total)
+}
+forJugFol(12, 19, 33, 57, 45)
