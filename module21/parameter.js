c74f11596773c1810215e87e83d4ec6c63db778b
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-02 20:01:37 +0600

    some code added for borgo meter

--- module21/parameter.js
+++ module21/parameter.js
@@ -4,3 +4,10 @@ function square(number){
     console.log('this is borgo meter',borgo)
 }
 square(50)
+console.log('-------------');
+function borgoMeter(num1,num2){
+    const total = num1 + num2;
+    console.log(total);
+    console.log('this is borgo meter', total)
+}
+borgoMeter(20,77)
