e299621ae8f2aad7654f551261231c23b353fd91
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-06 22:55:14 +0600

    some code added this commit j

---
+++ module21/array-operation.js
@@ -0,0 +1,25 @@
+function sumNumbers(number){
+  let sum = 0;
+  for(const numbers of number ){
+    console.log(number)
+    sum = sum + number;
+  }
+  return sum;
+}
+const number = [5, 3, 7, 10];
+const sum =sumNumbers(number);
+console.log('this is total number', sum)
+
+
+function foyejKhan(num){
+  let sum = 0;
+  for(const foyej of num ){
+    console.log(num);
+    const khan = sum + num;
+    return sum;
+
+  }
+}
+const numbs = [10, 15, 20, 30];
+const fKhan =foyejKhan(numbs);
+console.log(fKhan)
