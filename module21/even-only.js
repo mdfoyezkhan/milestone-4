94700b055d4bf91b2683741c217533d493d5259d
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-08 23:23:40 +0600

    some code added this commit

--- module21/even-only.js
+++ module21/even-only.js
@@ -9,6 +9,21 @@ function onlyEvenNumbers(numbers){
     }
     return sum;
 }
-const numbers =[11, 23, 31, 39]
+const numberss =[11, 23, 31, 39]
+const results = onlyEvenNumbers(numberss)
+console.log('this is only for even numbers', results)
+
+function onlyEvenNumbers(numbers){
+    let sum =0;
+    for(const number of numbers){
+        console.log(number)
+        if(number %2 === 0){
+            console.log(number)
+            sum = sum + number;
+        }   
+    }
+    return sum;
+}
+const numbers =[12, 14, 50, 60]
 const result = onlyEvenNumbers(numbers)
 console.log('this is only for even numbers', result)
