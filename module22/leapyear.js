ee2a3151267924d2f88bbded2fa11988cdd30d88
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-09 22:36:06 +0600

    some code added this commit

--- module22/leapyear.js
+++ module22/leapyear.js
@@ -13,9 +13,16 @@ function isLearyear2(year){
     if(year % 100 !== 0 && year % 4 === 0){
         return true;
     }
+    else if(year % 100 ===0 && year % 400 === 0){
+        return true;
+
+    }
     else{
         return false;
     }
 }
-const islear = isLearyear2(2025);
-console.log('hello world',islear);
+const islear1 = isLearyear2(2025);
+const islear2 = isLearyear2(2022);
+const islear3 = isLearyear2(2019);
+const islear4 = isLearyear2(2018);
+console.log('hello world',islear1, islear2, islear3, islear4);
