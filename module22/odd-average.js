9143cc7d40b0c9a2386555b5f2ad2d633a690d56
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-10 22:45:15 +0600

    even shonkar gor berkora holo

--- module22/odd-average.js
+++ module22/odd-average.js
@@ -1,7 +1,7 @@
 function oddAverege(numbers){
     const odds = [];
     for( const number of numbers){
-        if(number % 2 === 1){
+        if(number % 2 === 0){
             // console.log(number)
             odds.push(number)
         }
@@ -11,7 +11,10 @@ function oddAverege(numbers){
     for(const number of odds){
         sum = sum + number;
     }
-    console.log(sum)
+    const count = odds.length;
+    console.log(sum, count)
+    const avg = sum / count;
+    return avg;
 }
 const numbers = [42, 13, 58, 65,81, 96, 7];
 const avg = oddAverege(numbers);
