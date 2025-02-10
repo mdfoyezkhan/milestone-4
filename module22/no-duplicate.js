4ca9944cafc623a13adc2aaf462d1b6afc279e28
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-10 23:20:23 +0600

    new file on-duplicate.js with some code added this commit

---
+++ module22/no-duplicate.js
@@ -0,0 +1,14 @@
+const biryaniKhor = ['abul', 'babul', 'cabul', 'abul', 'babul', 'dabul', 'kabul', 'cabul'];
+const number = [1, 5, 61, 5, 87, 7, 5, 81, 61];
+function noDuplicate(array){
+    const unique = [];
+    console.log(array)
+    for(const items of array){
+        if( unique.includes(items) === false){
+            unique.push(items)
+        }
+    }
+    return unique;
+}
+const uniquearray = noDuplicate(biryaniKhor)
+console.log(uniquearray)
