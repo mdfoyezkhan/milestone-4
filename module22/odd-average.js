f53e988d4703bdd66151ed4d392df53af4e34722
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-10 22:35:27 +0600

    some code added this commit

--- module22/odd-average.js
+++ module22/odd-average.js
@@ -1,9 +1,17 @@
 function oddAverege(numbers){
+    const odds = [];
     for( const number of numbers){
-        if(number % 2 === 0){
-            console.log(number)
+        if(number % 2 === 1){
+            // console.log(number)
+            odds.push(number)
         }
     }
+    console.log(odds);
+    let sum = 0;
+    for(const number of odds){
+        sum = sum + number;
+    }
+    console.log(sum)
 }
 const numbers = [42, 13, 58, 65,81, 96, 7];
 const avg = oddAverege(numbers);
