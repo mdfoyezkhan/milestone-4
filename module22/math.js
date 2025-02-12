bef21c3300a0079befba635d4931a811f633e0c2
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-12 23:02:19 +0600

    some code added this commit

--- module22/math.js
+++ module22/math.js
@@ -1,6 +1,20 @@
-const Math ={
-    min: function min(num1, num2){
-        return num2;
-    }
-}
-console.log(Math.min);
+// const Math ={
+//     min: function min(num1, num2){
+//         return num2;
+//     }
+// }
+// console.log(Math.min);
+
+const min = Math.min(45, 21, 65, 126, 5, -99, 987);
+console.log(min);
+const max = Math.max(45, 21, 65, 126, 5, -99, 987);
+console.log(max)
+console.log(Math.PI)
+console.log(Math.abs(7-14))
+console.log(Math.round(5.54))
+console.log(Math.floor(6.99))
+console.log(Math.ceil(4.3))
+// console.log(Math.round(Math.random()*10))
+const rend = Math.round(Math.random()*10);
+console.log(rend)
+
