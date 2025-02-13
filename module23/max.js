88ac2c2ae9eb6142c6eddd35070a5708d47ac3d6
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-13 22:17:49 +0600

    new file max.js added this commit

---
+++ module23/max.js
@@ -0,0 +1,19 @@
+const foyej = 79;
+const salman = 98;
+if(foyej > salman){
+    console.log('foyej will get phone');
+}
+else{
+console.log('salman will get phone');
+}
+
+function maxMark(num1, num2){
+    if(num1 > num2){
+        return num2;
+    }
+    else{
+        return num1;
+    }
+}
+const maxMarks = maxMark(78, 9)
+console.log(maxMarks)
