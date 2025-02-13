b5ecd0846e927063ed6382a3e4a6e2b52514ba5c
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-13 23:40:14 +0600

    some code added in tallest.js file

--- module23/tallest.js
+++ module23/tallest.js
@@ -10,3 +10,17 @@ return max;
 }
 const max = maxSonka(heights);
 console.log('max value is',max)
+
+//----------------
+const foyej = [12, 13, 14, 34, 56, 78, 37]
+function minNumber(num){
+    let min =num[0];
+    for(const nam of num){
+        if(num < min){
+            min = num;
+        }
+    }
+    return min;
+}
+const minNumbers = minNumber(foyej);
+console.log('this is min number', minNumbers)
