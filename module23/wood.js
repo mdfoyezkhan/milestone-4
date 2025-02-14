a54810cd475404aded44c7bf05dbb34f01f6eee5
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-14 23:24:46 +0600

    new file wood.js added with some code

---
+++ module23/wood.js
@@ -0,0 +1,12 @@
+function woodQuantity(chair, table, bed){
+    const chairNumber = 4;
+    const tableNumber = 5;
+    const bedNumber = 2;
+    const chairTotalWood = chair * chairNumber;
+    const tableTotalWood = table * tableNumber;
+    const bedTotalNumber = bed * bedNumber;
+    // const totalKat = ;
+    return chairTotalWood, tableTotalWood, bedTotalNumber;
+}
+const wood = woodQuantity(0, 0, 1)
+console.log(wood)
