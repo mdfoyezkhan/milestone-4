b160d4b9dc96ccba4c499f5e29475f3b39a79b4f
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-18 22:38:29 +0600

    some code added

--- module24.5/object.js
+++ module24.5/object.js
@@ -4,10 +4,14 @@ const student = {
     address:'movie cinema',
     isSingle:true,
     friends:['foyej khan','emon khan', 'najmul khan'],
+    act: function(){
+        console.log('acting like shakib khan')
+    },
     car:{
         brand:'tesla',
         price: 500000000,
         mode:2025,
     }
 }
-console.log(student.friends)
+// console.log(student.friends)
+console.log(student.act())
