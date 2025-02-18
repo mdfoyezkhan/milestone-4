5f3960754fda7a3af464e4a996411c02db25380b
mdfoyezkhan <mdfoyezkn@gmail.com>
2025-02-18 22:07:51 +0600

    new file added this commit

---
+++ module24.5/object.js
@@ -0,0 +1,13 @@
+const student = {
+    name:'salman khan',
+    id:121,
+    address:'movie cinema',
+    isSingle:true,
+    friends:['foyej khan','emon khan', 'najmul khan'],
+    car:{
+        brand:'tesla',
+        price: 500000000,
+        mode:2025,
+    }
+}
+console.log(student.friends)
